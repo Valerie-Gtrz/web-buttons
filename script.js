abb3fe90bef9e94d85465ed54239b5c9e2571f63
Valerie Gutierrez <Valeriengutierrez@yahoo.com>
2021-07-04 00:01:10 -0500

    inital commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function iCannotLie(){
+    document.getElementById("message").style.display ='block';
+}
+
+function stopClick(){
+    document.getElementById("messageTwo").style.display ='block';
+}
